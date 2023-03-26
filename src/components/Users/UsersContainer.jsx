73de23106f256c0e8e2cd17753e1bb0113f56ed1
Dastinss <sergeyfdaragan@gmail.com>
2023-03-26 20:32:45 +0300

    #55 done. FIXED double х 6 Users info!!

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Users from "./Users";
 import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
+import usersReducer, {
+    followAC,
+    setCurrentPageAC,
+    setTotalUsersCountAC,
+    setUsersAC,
+    unfollowAC
+} from "../../redux/users-reducer";
 import users from "./Users";
 
-let mapStateToProps = ( state ) => {
+let mapStateToProps = ( state ) => { //наша компонента через пропсы получит значения ниже (они первоначально формируются в usersReducer)
     return {
-        users: state.usersPage.users // с помощью mapStateToProps придет в ф-циональную компоненту Users в пропсах будет сидеть св-во users: значением которого будут пользователи из стейта (.users)
+        users: state.usersPage.users, // с помощью mapStateToProps придет в ф-циональную компоненту Users в пропсах будет сидеть св-во users: значением которого будут пользователи из стейта (.users)
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage //чтобы наша компонента в пропсах получила это значение
     }
 }
 
-let mapDispatchToProps = ( dispatch ) => { //ф-ция которая служит для того, чтобы передавать в ф-циональную компоненту Users через пропсы колл-беки (назначение которых общаться со стейтом), т.е. какие то ф-ции, которые она сможет вызывать
+let mapDispatchToProps = ( dispatch ) => { //ф-ция которая приходит из react-redux библиотеки, задача которой скрыть нам store, subscribe, dispatch, т.е. упрощает єтот мех=зм. Она служит для того, чтобы передавать в ф-циональную компоненту Users через пропсы колл-беки (назначение которых общаться со стейтом), т.е. какие то ф-ции, которые она сможет вызывать
     return {
         folllow: ( userId ) => { // это ф-ция которая будет диспатчить акшн криэйтер указывая какого пользователя нужно зафоловить
             dispatch( followAC( userId ) ); // мы диспатчим результат работы АС , который вызывает нам action
@@ -22,8 +31,27 @@ let mapDispatchToProps = ( dispatch ) => { //ф-ция которая служи
 
         setUsers: ( users ) => { //человек попадает на страничку, которая (презент.компонента) берет откудато пользователей, задиспатчивает их в стейт, а потом когда они приходят она их отрисовывает
             dispatch( setUsersAC( users ) );
+        },
+
+        setCurrentPage: ( pageNumber ) => {
+            dispatch( setCurrentPageAC( pageNumber ) );
+        },
+
+        setTotalUsersCount: ( totalCount ) => {
+            dispatch( setTotalUsersCountAC( totalCount ) );
         }
+
+
     }
 }
 
 export default connect( mapStateToProps, mapDispatchToProps )( Users );
+//детально описывается характер работы в уроке 55 на 00:55:00 - 01:02:00 минутах
+// наш Users, т.е. наша компонента ты получишь все эти пропсы с помощью connect и мы закинем в тебя эти все пропсы
+// с помощью этих двух супер ф-ций ( mapStateToProps, mapDispatchToProps ):
+// mapStateToProps - возвращает объект который нам по итогу из стейта достает свойства, значения, данные
+// mapDispatchToProps - ф-ция возвращающая обьект, в котором есть колбеки ,скажем так. Каждый колл бек диспатчит что то по итогу в стор и там что то происходит.
+// Что то там произошло и заново срабатывает ф-ция mapStateToProps
+// connect - библиотека\ф-ция, с помощью которая и скрывают store, subscribe, dispatch
+
+
--- src/redux/users-reducer.jsx
+++ src/redux/users-reducer.jsx
@@ -1,51 +1,67 @@
-// Єто у нас BLL уровень !!!
+// Єто у нас BLL уровень !!! Єто бизнес логика - ОТ НЕЕ ОТТАЛКИВАЕМСЯ!!! UI (Users) - єто уже производная от BLL
 
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_CURRENT_USERS_COUNT = 'SET_CURRENT_USERS_COUNT';
 
 let initialState = {
-    users: [ ]
+    users: [], // в state есть пустой массив в нем пользователи кот счас нет и которых мы запрашиваем с сервера и потом сюда сетаем
+    pageSize: 5, // захардкодили количество страниц
+    totalUsersCount: 0, // мы не знаем сколько юзеров, пока не уйдет запрос на сервер и пока мы не узнаем ответ
+    currentPage: 1 // текущая страница (которая подсвечивается среди прочихЖ 12345)
 };
 
 const usersReducer = ( state = initialState, action ) => {
 
-        switch (action.type) {
-            case FOLLOW:
-                return {
-                    ...state,
-                    // users: [...state.users] - создаем копию, но лучше делать через мар, т.к. нам нужно изменить только один обїект, а вытащить его проще через мар
-                    users: state.users.map( u => {
-                        if (u.id === action.userId) {
-                            return { ...u, followed : true } // если ID совпадает, то возвращаем копию обекта, если не совпадает - то возвращаем сам обїект
-                        }
-                        return u;
-                    } )
-                }
-
-            case UNFOLLOW :
-                return {
-                    ...state,
-                    users: state.users.map ( u => {
-                        if (u.id === action.userId) {
-                            return { ...u, followed: false }
-                        }
-                        return u;
-                    })
-                }
-
-            case SET_USERS : {
-                // return { ...state, users: action.users } // если откуда то придут пользователи, то мы должны взять старый стейт, взять пользователей которые там были и перезатереть их теми пользователямя, которые пришли в action
-                return { ...state, users: [ ...state.users, ...action.users ] }// если откуда то придут пользователи, то мы должны взять старый стейт, взять пользователей которые там были и ДОБАВИТЬ (склеить) к ним тех пользователей, которые пришли в action
+    switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                // users: [...state.users] - создаем копию, но лучше делать через мар, т.к. нам нужно изменить только один обїект, а вытащить его проще через мар
+                users: state.users.map( u => {
+                    if (u.id === action.userId) {
+                        return { ...u, followed: true } // если ID совпадает, то возвращаем копию обекта, если не совпадает - то возвращаем сам обїект
+                    }
+                    return u;
+                } )
             }
 
-            default:
-                return state;
+        case UNFOLLOW :
+            return {
+                ...state,
+                users: state.users.map( u => {
+                    if (u.id === action.userId) {
+                        return { ...u, followed: false }
+                    }
+                    return u;
+                } )
+            }
+
+        case SET_USERS : {
+            // return { ...state, users: action.users } // если откуда то придут пользователи, то мы должны взять старый стейт, взять пользователей которые там были и перезатереть их теми пользователямя, которые пришли в action
+            return { ...state, users: action.users }// если откуда то придут пользователи, то мы должны взять старый стейт, взять пользователей которые там были и ДОБАВИТЬ (склеить) к ним тех пользователей, которые пришли в action
         }
-    };
+
+        case SET_CURRENT_PAGE: {
+            return { ...state, currentPage: action.currentPage }// меняем currentPage на тот Page который сидит в action
+        }
+
+        case SET_CURRENT_USERS_COUNT: {
+            return { ...state, totalUsersCount: action.count }// меняем currentPage на тот Page который сидит в action
+        }
+
+        default:
+            return state;
+    }
+};
 
 export const followAC = ( userId ) => ({ type: FOLLOW, userId }); // AC = ActionCreator ф-ция которая формирует и возвращает action
 export const unfollowAC = ( userId ) => ({ type: UNFOLLOW, userId });
 export const setUsersAC = ( users ) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = ( currentPage ) => ({ type: SET_CURRENT_PAGE, currentPage });
+export const setTotalUsersCountAC = ( totalUsersCount ) => ({ type: SET_CURRENT_USERS_COUNT, count : totalUsersCount });
+
 
 export default usersReducer;
