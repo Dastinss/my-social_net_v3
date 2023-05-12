1ed2138484ad1583825e999371d999d43fb5a903
Dastinss <sergeyfdaragan@gmail.com>
2023-05-12 22:26:24 +0300

    #75 done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { formToJSON } from "axios";
+import { Field, reduxForm } from "redux-form";
 
-const Login = (props) => {
-    return <h1>LOGIN</h1>
+const LoginForm = ( props ) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                  <Field placeholder={'Login'} name={'login'} component={'input'}/>{/*#75 добавил имя name={УКАЗЫВАЕМ свойство} для отправки на сервер имени данного элемента куда мы вводим данные*/}
+                {/*<input placeholder={'Login'}/> // #75 заменили на Field (по сути контейнерная компонента. которая рисует др компоненту) из билиотеки reduxForm*/}
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'password'} component={'input'}/>
+                {/*<input placeholder={'Password'} // #75 заменили на Field (по сути контейнерная компонента. которая рисует др компоненту)из билиотеки reduxForm/>*/}
+            </div>
+            <div>
+                <Field component={'input'} name={'rememberMe'} type={'checkbox'}/> remember me
+                {/*<input type={'checkbox'}/> remember me // #75 заменили на Field (по сути контейнерная компонента. которая рисует др компоненту)из билиотеки reduxForm/>*!/*/}
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+};
+
+const LoginReduxForm = reduxForm({ // <----- THIS IS THE IMPORTANT PART! #73 скопировал из сайта ReduxForm. Это HOC, которой мы оборачиваем этой контейнерной компонентой, она прокидывает особые пропсы в нашу форму, особый кол-бэк
+                  form: 'login',                                // a unique name for this form, ЄТО имя никак не связано с словом form в redux-store.jsx, где оно означает название стейта в глобальном нашем общем стейте с которым работает formReduser
+                  // fields: ['firstName', 'lastName', 'email'] // all the fields in your form
+              })(LoginForm);                                    // передаем форму вокруг которой нужно создать эту РедаксФорм
+
+const Login = ( props ) => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit = {onSubmit}/> {/*// ресуем контейнерную компоненту, которую мы получили*/}
+    </div>
 };
 
 export default Login;
--- src/redux/redux-store.jsx
+++ src/redux/redux-store.jsx
@@ -5,13 +5,15 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk" // промежуточный уровень , который мы внедряем в наш стор
+import {reducer as formReducer} from 'redux-form';
 
 let reducers = combineReducers({ // єто ф-ция, которая соединяет четыре наших редьюсера (т.е. четыре ВЕТКИ нашего стейта , т.е. св-ва каждое из которыъ яв-ся методом), т.е. которой мы передаем объект (ключ: значение)
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer, // ветка usersPage обслуживается usersReducer_ом
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer // #75 редьюсер formReduser (установленная библиотека) должен называться обязательно form:
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware)); // создаем редаксовский стор с помощью этой ф-ции которой передаем закомбайененные редьюсеры, в уроке 66 добавили applyMiddleware для редакс thunk
@@ -19,5 +21,3 @@ let store = createStore(reducers, applyMiddleware(thunkMiddleware)); // созд
 window.store = store;
 
 export default store;
-
-
