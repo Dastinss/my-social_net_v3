4e30aab33aa74e062caf48dad1c562fdf57eca64
Dastinss <sergeyfdaragan@gmail.com>
2023-12-01 20:39:10 +0200

    feat: #94 done: added React.lazy и React.Suspense to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 // import store from "./redux/store";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginPage from "./components/Login/Login";
+// import DialogsContainer from "./components/Dialogs/DialogsContainer"; //94 закоментили т.к. меняем на "ленивую загрузку"
+// import UsersContainer from "./components/Users/UsersContainer"; //94 закоментили т.к. меняем на "ленивую загрузку"
+// import ProfileContainer from "./components/Profile/ProfileContainer"; //94 закоментили т.к. меняем на "ленивую загрузку"
+// import LoginPage from "./components/Login/Login"; //94 закоментили т.к. меняем на "ленивую загрузку"
 import { connect, Provider } from "react-redux";
 import { getAuthUserData } from "./redux/auth-reducer";
 import { compose } from "redux";
 import { initializeApp } from "./redux/app-reducer";
 import Preloader from "./components/Common/Preloader/Preloader";
 import store from "./redux/redux-store";
+
+const DialogsContainer = lazy( () => import('./components/Dialogs/DialogsContainer') ); //94 запускаем "ленивую загрузку" компоненты DialogsContainer с https://react.dev/reference/react/lazy
+const UsersContainer = lazy( () => import("./components/Users/UsersContainer"));//94 запускаем "ленивую загрузку" компоненты DialogsContainer с https://react.dev/reference/react/lazy
+const  ProfileContainer = lazy( () => import("./components/Profile/ProfileContainer"));//94 запускаем "ленивую загрузку" компоненты DialogsContainer с https://react.dev/reference/react/lazy
+const  LoginPage = lazy( () => import("./components/Login/Login"));//94 запускаем "ленивую загрузку" компоненты DialogsContainer с https://react.dev/reference/react/lazy
 
 
 class App extends React.Component { // 80 через гарячие клавиши сделали из функциональной классовую кампоненту,т.к. нам нужем метод жизненного цикла. все сноски оставил
@@ -32,34 +37,37 @@ class App extends React.Component { // 80 через гарячие клавиш
 				<HeaderContainer/>
 				<Navbar/>
 				<div className='app-wrapper-content'>
-					<Routes>
-						<Route path="/dialogs"
-							// element={<Dialogs store={props.store} // закоментили в уроке 43 когда создали контейнерную компоненту DialogsContainer
-							// element={<DialogsContainer store={props.store} // закоментили в уроке 44 когда создали контейнерную компоненту StoreContext перестали передавать в нашу контейнерную компоненту что либо через пропсы т.е. дали доступ store ко всем компонентам внутри App
-							   element={<DialogsContainer/>}/>
-						<Route
-							path="/profile/:userId?" // после урока 59 добавил /* в Route path="/profile/*" из комментов к уроку 59, т.к. без этого не отображался корректно добавляемый user
-							// element={<Profile store = {props.store} // закоментили в уроке 44 когда создали контейнерную компоненту StoreContext перестали передавать в нашу контейнерную компоненту что либо через пропсы т.е. дали доступ store ко всем компонентам внутри App
+					<React.Suspense fallback={<div><Preloader/></div>}>
+						<Routes>
+							<Route path="/dialogs"
+								// element={<Dialogs store={props.store} // закоментили в уроке 43 когда создали контейнерную компоненту DialogsContainer
+								// element={<DialogsContainer store={props.store} // закоментили в уроке 44 когда создали контейнерную компоненту StoreContext перестали передавать в нашу контейнерную компоненту что либо через пропсы т.е. дали доступ store ко всем компонентам внутри App
+								   element={<DialogsContainer/>}/>
 
-							// profilePage={props.state.profilePage} // закоментили в уроке 43 когда создали контейнерную компоненту MyPostsContainer
-							// addPost={props.addPost}
-							// updateNewPostText={props.updateNewPostText}/>}/>
+							<Route
+								path="/profile/:userId?" // после урока 59 добавил /* в Route path="/profile/*" из комментов к уроку 59, т.к. без этого не отображался корректно добавляемый user
+								// element={<Profile store = {props.store} // закоментили в уроке 44 когда создали контейнерную компоненту StoreContext перестали передавать в нашу контейнерную компоненту что либо через пропсы т.е. дали доступ store ко всем компонентам внутри App
 
-							//заменили закоменченные выше два метода на один dispatch
-							// dispatch={props.dispatch} // закоментили в уроке 43 когда создали контейнерную компоненту MyPostsContainer
-							//    element={<Profile/>}/> // закоментили в уроке 59 когда создали контейнерную компоненту для Profile
-							element={<ProfileContainer/>}/>
+								// profilePage={props.state.profilePage} // закоментили в уроке 43 когда создали контейнерную компоненту MyPostsContainer
+								// addPost={props.addPost}
+								// updateNewPostText={props.updateNewPostText}/>}/>
 
-						<Route path="/users"
-						       element={<UsersContainer/>}/>
+								//заменили закоменченные выше два метода на один dispatch
+								// dispatch={props.dispatch} // закоментили в уроке 43 когда создали контейнерную компоненту MyPostsContainer
+								//    element={<Profile/>}/> // закоментили в уроке 59 когда создали контейнерную компоненту для Profile
+								element={<ProfileContainer/>}/>
 
-						<Route path="/login"
-						       element={<LoginPage/>}/>
-					</Routes>
+							<Route path="/users"
+							       element={<UsersContainer/>}/>
+
+							<Route path="/login"
+							       element={<LoginPage/>}/>
+						</Routes>
+					</React.Suspense>
 				</div>
 			</div>
-			// </BrowserRouter>
-		)
+	// </BrowserRouter>
+	)
 	}
 }
 
@@ -67,7 +75,7 @@ const mapStateToProps = ( state ) => ({
 	initialized: state.app.initialized // 80 ф-ция получила стейт и вернула обьект в котором будет initialized Теперь наш арр получит знания про то проинициализировано оно или нет
 })
 
-let AppContainer =  compose( // 92 создали переменную и присвоили ей значения с урока 80
+let AppContainer = compose( // 92 создали переменную и присвоили ей значения с урока 80
 	connect( mapStateToProps, { initializeApp } ) )( App );
 
 let SamuraiJSApp = ( props ) => { // 92 создали главную Арр-шку, которая будет делать все что делали в index.js
